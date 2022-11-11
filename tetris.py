8c2f230126330cd57c485e6336d5a6e189b24dc2
clcristianem <117388579+clcristianem@users.noreply.github.com>
2022-11-11 11:32:45 -0500

    Create tetris.py
    Tetris with a twist! New blocks added to make the game more challenging and fun!

---
+++ tetris.py
@@ -0,0 +1,50 @@
+import pygame
+import random
+
+
+#Game info
+#Creates arcade look: Main screen is bigger than play screen
+screen_width = 1000
+screen_height = 900
+play_width = 400
+play_height = 800
+
+#Start with normal tetris blocks, then create new blocks
+#New block ideas - "H" block, extended "O" block, "U" block, create more ideas later
+
+#BLOCK SHAPES
+#S BLOCK
+S = [['.....',
+      '.....',
+      '..00..'
+      '.00...'
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+#I BLOCK
+
+#Z BLOCK
+
+#J BLOCK
+
+#O BLOCK
+
+#L BLOCK
+
+#T BLOCK
+
+#H BLOCK
+
+#U BLOCK
+
+#EXTO BLOCK
+
+#SHAPE INFO
+
+#CREATE GRID
+
+#CREATE RANDOM SHAPES
